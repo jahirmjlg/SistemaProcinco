8e349c3efb3b14174f163a2d98d26e415e73b588
jahirmjlg <161340354+jahirmjlg@users.noreply.github.com>
2024-04-30 00:32:31 -0600

    repositorio de usuarios
    repositorio de usuarios sin terminar

--- SistemaProcinco/SistemaProcinco.DataAccess/Repository/UsuariosRepository.cs
+++ SistemaProcinco/SistemaProcinco.DataAccess/Repository/UsuariosRepository.cs
@@ -1,7 +1,10 @@
-﻿using SistemaProcinco.DataAcces.Repository;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SistemaProcinco.DataAcces.Repository;
 using SistemaProcinco.Entities.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +15,54 @@ namespace SistemaProcinco.DataAccess.Repository
     {
         public RequestStatus Delete(int? id)
         {
-            throw new NotImplementedException();
+            string sql = ScriptsDatabase.UsuariosEliminar;
+
+            using (var db = new SqlConnection(SistemaProcincoContext.ConnectionString))
+            {
+                var parametro = new DynamicParameters();
+                parametro.Add("Usua_Id", id);
+                var result = db.Execute(sql, parametro, commandType: CommandType.StoredProcedure);
+
+                return new RequestStatus { CodeStatus = result, MessageStatus = "" };
+
+            }
         }
 
         public IEnumerable<tbUsuarios> Find(int? id)
         {
-            throw new NotImplementedException();
+            string sql = ScriptsDatabase.UsuariosDetalle;
+            List<tbUsuarios> result = new List<tbUsuarios>();
+
+            using (var db = new SqlConnection(SistemaProcincoContext.ConnectionString))
+            {
+                var parametro = new DynamicParameters();
+                parametro.Add("@Usua_Id", id);
+                result = db.Query<tbUsuarios>(sql, parametro, commandType: CommandType.StoredProcedure).ToList();
+
+                return result;
+
+            }
         }
 
         public RequestStatus Insert(tbUsuarios item)
         {
-            throw new NotImplementedException();
+            string sql = ScriptsDatabase.UsuariosCrear;
+
+            using (var db = new SqlConnection(SistemaProcincoContext.ConnectionString))
+            {
+                var parametro = new DynamicParameters();
+                parametro.Add("@Usuario", item.Usua_Usuario);
+                parametro.Add("@UsuaClave", item.Usua_Contraseña);
+                parametro.Add("@UsuaIsAdmin", item.Usua_EsAdmin);
+                parametro.Add("@Rol_Id", item.Role_Id);
+                parametro.Add("@UsuaCreacion", item.Usua_UsuarioCreacion);
+                parametro.Add("@FechaCreacion", item.Usua_FechaCreacion);
+                parametro.Add("@Correo", item.Empl_Id);
+                var result = db.Execute(sql, parametro, commandType: CommandType.StoredProcedure);
+                string mensaje = (result == 1) ? "exito" : "error";
+                return new RequestStatus { CodeStatus = result, MessageStatus = mensaje };
+
+            }
         }
 
         public IEnumerable<tbUsuarios> List()
