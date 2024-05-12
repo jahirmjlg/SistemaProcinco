b59167004b0c910a0f476ee1ba03d1e09b7ad0ca
FernanJS17 <109115569+FernanJS17@users.noreply.github.com>
2024-05-12 10:04:30 -0600

    dx

--- PlantillaProcinco/src/app/auth.guard.ts
+++ PlantillaProcinco/src/app/auth.guard.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './Services/authGuard.service';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
     providedIn: 'root'
   })
   export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router,  private cookieService: CookieService) {}
+
 
     canActivate(
       next: ActivatedRouteSnapshot,
       state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       const url: string = state.url;
+
+      const admin = this.cookieService.get('esAdmin').toString()
+
+      if (admin != "true"){
+          return true;
+      }
+
+
       if (!this.authService.isUrlAllowed(url)) {
         this.router.navigate(['/login']);
         return false;
